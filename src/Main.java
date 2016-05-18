bca26d8dd81493d867015849dbc5c98903ff7529
filipemleite94 <filipemleite94@gmail.com>
2016-05-18 01:10:02 -0300

    Versao2.0
    Nova versao

--- src/Main.java
+++ src/Main.java
@@ -1,138 +1,113 @@
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Main extends JPanel implements ActionListener, KeyListener {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	Timer t = new Timer(5, this);
-	public int x = 0, y = 300;
+	public int x = 0, y = 50;
 	public static int velx = 0;
-	public int vely = 0;
-	public int i=0;
-	public int j=0;
-	public int xb = 0, yb = 500;
+	public int xa = 0, ya = 250;
+	public static int velxa = 0;
+	public int xb = 0, yb = 450;
 	public static int velxb = 0;
-	public int velyb = 0;
-	public int ib=0;
-	public int jb=0;
-	
+	public int xc = 0, yc = 650;
+	public static int velxc = 0;
+	int colocacao=0, player1=0, player2=0, player3=0, player4=0;
+	boolean emJogo=true;
+	private Image player1icon, player2icon, player3icon, player4icon;
+
 	public Main() {
 		t.start();
 		addKeyListener(this);
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
+		ImageIcon referencia = new ImageIcon(Main.class.getResource("Link.png"));
+		player1icon=player2icon=player3icon=player4icon=referencia.getImage();;
 	}
 	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.setColor(Color.RED);
-		g.fillRect(x, y, 50, 30);
-		g.setColor(Color.BLUE);
-		g.fillRect(xb, yb, 50, 30);
+	public void paint(Graphics g) {
+		Graphics2D graficos = (Graphics2D) g;
+
+		if (emJogo) {
+			graficos.drawImage(player1icon, x, y, this);
+			graficos.drawImage(player2icon, xa, ya, this);
+			graficos.drawImage(player3icon, xb, yb, this);
+			graficos.drawImage(player4icon, xc, yc, this);
+		}
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		if (x < 0) {
-			velx = 0;
-			x = 0;
-		}
 
 		if (x > 1130) {
 			velx = 0;
 			x = 1130;
+			colocacao++;
+			player1=colocacao;
 		}
 
-		if (y < 0) {
-			vely = 0;
-			y = 0;
-		}
+		if(player1==0) x += velx;
 
-		if (y > 730) {
-			vely = 0;
-			y = 730;
+		if (xa > 1130) {
+			velxa = 0;
+			xa = 1130;
+			colocacao++;
+			player2=colocacao;
 		}
-
-		x += velx;
-		y += vely;
 		
-		if (xb < 0) {
-			velxb = 0;
-			xb = 0;
-		}
-
+		if(player2==0) xa += velxa;
+		
 		if (xb > 1130) {
 			velxb = 0;
 			xb = 1130;
+			colocacao++;
+			player3=colocacao;
 		}
-
-		if (yb < 0) {
-			velyb = 0;
-			yb = 0;
-		}
-
-		if (yb > 730) {
-			velyb = 0;
-			yb = 730;
-		}
-
-		xb += velxb;
-		yb += velyb;
 		
+		if(player3==0)xb += velxb;
+		
+		if (xc > 1130) {
+			velxc = 0;
+			xc = 1130;
+			colocacao++;
+			player4=colocacao;
+		}
+		
+		if(player4==0)xc += velxc;
 		repaint();
+		if(colocacao>4) this.endgame();
 	}
-
+	
+	public void endgame(){
+		//botar resultados colocacao = player e tela de fim de jogo;
+		//pssivel reiniciarjogo
+	}
+	
 	public void keyPressed(KeyEvent e) {
 		int code = e.getKeyCode();
-
-		if (code == KeyEvent.VK_DOWN) {
-			vely = 1;
-			velx = 0;
+		if (code == KeyEvent.VK_Q) {
+			x += 20;
 		}
-		if (code == KeyEvent.VK_UP) {
-			vely = -1;
-			velx = 0;
+	
+		if (code == KeyEvent.VK_F) {
+			xa += 20;
 		}
-		if (code == KeyEvent.VK_LEFT) {
-			vely = 0;
-			x -= 20;
-			j--;
+		if (code == KeyEvent.VK_M) {
+			xb += 20;
 		}
 		if (code == KeyEvent.VK_RIGHT) {
-			vely = 0;
-			x += 20;
-			i++;
+			xc += 20;
 		}
-		
-		if (code == KeyEvent.VK_S) {
-			velyb = 1;
-			velxb = 0;
-		}
-		if (code == KeyEvent.VK_W) {
-			velyb = -1;
-			velxb = 0;
-		}
-		if (code == KeyEvent.VK_A) {
-			velyb = 0;
-			xb -= 20;
-			jb--;
-		}
-		if (code == KeyEvent.VK_D) {
-			velyb = 0;
-			xb += 20;
-			ib++;
-		}
-		
 	}
 
 	public void keyTyped(KeyEvent e) {
@@ -141,23 +116,12 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 	public void keyReleased(KeyEvent e) {
 	}
 
-	
-	final static int NUM_CARROS = 2; // QTE. de sapos na corrida
-	final static int DISTANCIA = 1200; // Dist�ncia da corrida em cm
-
 	public static void main(String[] args) {
-
 		JFrame f = new JFrame();
 		Main s = new Main();
 		f.add(s);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(1200,800);
+		f.setSize(1200,750);
 		f.setVisible(true);
-		
-		/* colocando carros na corrida */
-		for (int i = 1; i <= NUM_CARROS; i++) {
-			CarroCorrendoThread c = new CarroCorrendoThread("CARRO_" + i, DISTANCIA, i, s);
-			new Thread(c).start();
-		}
 	}
 }
